b91cd919aca664b6cf9b25a4eac1433a96c4dbe6
Nhlalo <nhlalonkosi@gmail.com>
2025-09-09 20:47:08 +0200

    Implement landing page component
    - Create main page structure with two sections
    - Add feature items with icons and text descriptions
    - Use proper semantic HTML elements

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,20 +1,47 @@
 import { useState } from "react";
 import { Link } from "react-router";
+import { Music2, Share, File } from "lucide-react";
 
 function Homepage() {
   return (
-    <header>
-      <div>
-        <img src="../assets/Logo.PNG" alt="Musicfier" />
-        <h1>Musicfier</h1>
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <Link>My Library</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header>
+        <div>
+          <img src="../assets/Logo.PNG" alt="Musicfier" />
+          <h1>Musicfier</h1>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link>My Library</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <section>
+          <h2>Tap To Musicfy</h2>
+          <button type="button">
+            <img src="../assets/SearchImage.PNG" alt="search" />
+          </button>
+        </section>
+        <section>
+          <ul>
+            <li>
+              <Music2 />
+              <p>Real-time music recognition</p>
+            </li>
+            <li>
+              <File />
+              <p>Song history</p>
+            </li>
+            <li>
+              <Share />
+              <p>Share across platforms</p>
+            </li>
+          </ul>
+        </section>
+      </main>
+    </>
   );
 }
