45b512806e3a622c5dcadcf519bf1c039d7225a0
Nhlalo <nhlalonkosi@gmail.com>
2025-09-11 17:29:21 +0200

    Structure the searching page

---
+++ src/components/searchingpage/searchingpage.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Link } from "react-router";
+import SearchImage from "../../assets/searchimage.png";
+import { X } from "lucide-react";
+import { AudioLines } from "lucide-react";
+import { Ellipsis } from "lucide-react";
+import Styles from "./style.module.css";
+
+export default function Searchingpage() {
+  return (
+    <main>
+      <div>
+        <X />
+      </div>
+      <section>
+        <img src={SearchImage} alt="searching" />
+        <div>
+          <AudioLines />
+          <p>Listenign for music</p>
+          <p>Make sure your device can hear the song clearly</p>
+        </div>
+        <div>
+          <Ellipsis />
+          <p>Searching</p>
+          <p>Please wait</p>
+        </div>
+        <div>
+          <p>This is tough</p>
+          <p>Last try</p>
+        </div>
+      </section>
+    </main>
+  );
+}
