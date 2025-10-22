12ac6e31b2fb167ee511115a4edcc6c1a3f4f4aa
Nhlalo <nhlalonkosi@gmail.com>
2025-10-22 16:32:49 +0200

    Add and structure the artist description section

--- src/components/resultpage/aboutArtist/aboutArtist.jsx
+++ src/components/resultpage/aboutArtist/aboutArtist.jsx
@@ -15,6 +15,25 @@ export default function aboutArtist() {
           <span className={Styles.artistName}>Drake</span>
         </div>
       </section>
+      <section
+        aria-label="Description of the artist"
+        className={Styles.artistDescriptionContainer}
+      >
+        <h2 className={Styles.aboutWord}>ABOUT</h2>
+        <p className={Styles.artistDescription}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui
+          repellendus corporis excepturi, omnis exercitationem maiores
+          praesentium expedita libero incidunt nostrum minus quam velit iusto
+          nobis cupiditate facere harum alias numquam.
+          <button
+            type="button"
+            aria-label="Show more information"
+            className={Styles.moreBTN}
+          >
+            MORE
+          </button>
+        </p>
+      </section>
     </>
   );
 }
