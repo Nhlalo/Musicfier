ab367bf4aa1c40b37659105266131f5f0dcb44f5
Nhlalo <nhlalonkosi@gmail.com>
2026-01-29 22:10:42 +0200

    build: restructure the template file system

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
-import "./css/index.css";
+import "./styles/global.css";
 import router from "./route.jsx";
 
 createRoot(document.getElementById("root")).render(
