b829fe569606809c37f3bc9c0dc919d511543502
Nhlalo <nhlalonkosi@gmail.com>
2026-01-30 12:18:34 +0200

    feature: mock the geonames api response

---
+++ src/data/anylocation-mock.jsx
@@ -0,0 +1,39 @@
+const mockLocation = [
+  {
+    city: "Johannesburg",
+    country: "South Africa",
+    countryCode: "ZA",
+    key: crypto.randomUUID(), // Keys are needed for any list creation
+  },
+  {
+    city: "Spain",
+    country: "Barcelona",
+    countryCode: "ES",
+    key: crypto.randomUUID(), // Keys are needed for any list creation
+  },
+  {
+    city: "Egypt",
+    country: "Cairo",
+    countryCode: "EG",
+    key: crypto.randomUUID(), // Keys are needed for any list creation
+  },
+  {
+    city: "Lisbon",
+    country: "Portugal",
+    countryCode: "PT",
+    key: crypto.randomUUID(), // Keys are needed for any list creation
+  },
+  {
+    city: "Rosario",
+    country: "Argentina",
+    countryCode: "AR",
+    key: crypto.randomUUID(), // Keys are needed for any list creation
+  },
+  {
+    city: "London",
+    country: "Englan",
+    countryCode: "GB",
+    key: crypto.randomUUID(), // Keys are needed for any list creation
+  },
+];
+export default { mockLocation };
