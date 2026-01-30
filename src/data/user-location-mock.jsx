23ed2ace5ebfaa6ae189a4a2b2440474ea64f3ef
Nhlalo <nhlalonkosi@gmail.com>
2026-01-30 12:21:40 +0200

    feature: mock the user's location data

---
+++ src/data/user-location-mock.jsx
@@ -0,0 +1,8 @@
+const mockUserLocation = {
+  city: "Johannesburg",
+  country: "South Africa",
+  country_code: "ZA",
+  lat: -26.195246,
+  lon: 28.034088,
+};
+export default { mockUserLocation };
