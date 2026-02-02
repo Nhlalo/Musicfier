9def3c2fbefde64f512fdf08277544b230aeaa79
Nhlalo <nhlalonkosi@gmail.com>
2026-02-02 17:24:12 +0200

    fix: add return statement within the container component

--- src/components/ui/container/container.jsx
+++ src/components/ui/container/container.jsx
@@ -1,6 +1,8 @@
 import Styles from "./container.module.css";
 export default function Container({ children }) {
-  <section className={Styles.Container}>
-    <div className={Styles.wrapper}>{children}</div>
-  </section>;
+  return (
+    <section className={Styles.Container}>
+      <div className={Styles.wrapper}>{children}</div>
+    </section>
+  );
 }
