f5e45097434eb215b4ddde677a601e9f42b76480
Nhlalo <nhlalonkosi@gmail.com>
2026-02-02 14:36:39 +0200

    feat: import styles within container component

--- src/components/ui/container/container.jsx
+++ src/components/ui/container/container.jsx
@@ -1,3 +1,4 @@
+import Styles from "./container.module.css";
 export default function Container({ children }) {
   <section className={Styles.Container}>
     <div className={Styles.wrapper}>{children}</div>
