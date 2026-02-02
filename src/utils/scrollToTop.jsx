e176193404b6fae7b107469a1fa9c27d544bcc6e
Nhlalo <nhlalonkosi@gmail.com>
2026-02-02 18:30:58 +0200

    feat: create scroll to top handle

---
+++ src/utils/scrollToTop.jsx
@@ -0,0 +1,8 @@
+export default function handleScrollToTop(e) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth", // Optional: adds smooth scrolling
+  });
+}
