e7f50a5a30b8f56310c3dccdd521a66a5272d63c
Nhlalo <nhlalonkosi@gmail.com>
2026-02-03 22:07:06 +0200

    feat: mock the data from getNames library

---
+++ src/data/mock/allcountries-mock.jsx
@@ -0,0 +1,14 @@
+const mockCountries = [
+  { value: "United States", label: "United States" },
+  { value: "United Kingdom", label: "United Kingdom" },
+  { value: "Canada", label: "Canada" },
+  { value: "Australia", label: "Australia" },
+  { value: "Germany", label: "Germany" },
+  { value: "France", label: "France" },
+  { value: "Japan", label: "Japan" },
+  { value: "India", label: "India" },
+  { value: "Brazil", label: "Brazil" },
+  { value: "South Africa", label: "South Africa" },
+];
+
+export { mockCountries };
