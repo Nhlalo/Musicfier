49de09f97bf727bddcc373eb88a4c13cc80b9bd2
Nhlalo <nhlalonkosi@gmail.com>
2026-02-04 22:36:20 +0200

    feat: globalise the user's location uing context api

--- src/components/layout/rootLayout.jsx
+++ src/components/layout/rootLayout.jsx
@@ -1,8 +1,13 @@
+import { createContext, useState, useEffect } from "react";
 import { Outlet, useMatches } from "react-router";
+import { mockUserLocation } from "../../data/mock/user-location-mock";
 import Header from "./header/header";
 import Footer from "./footer/footer";
 
+const LocationContext = createContext();
 export default function RootLayout() {
+  const [location, setLocation] = useState(null);
+
   const matches = useMatches();
   const routeHandles = matches.map((match) => match.handle).filter(Boolean);
 
@@ -10,13 +15,21 @@ export default function RootLayout() {
   const shouldHideHeader = mergedHandle?.header === "hidden";
   const shouldHideFooter = mergedHandle?.footer === "hidden";
 
+  useEffect(() => {
+    const userLocation = mockUserLocation.country;
+    setLocation(userLocation);
+  }, []);
+
   return (
     <>
-      {!shouldHideHeader && <Header />}
-      <main>
-        <Outlet />
-      </main>
-      {!shouldHideFooter && <Footer />}
+      <LocationContext.Provider value={location}>
+        {!shouldHideHeader && <Header />}
+        <main>
+          <Outlet />
+        </main>
+        {!shouldHideFooter && <Footer />}
+      </LocationContext.Provider>
     </>
   );
 }
+export { LocationContext };
