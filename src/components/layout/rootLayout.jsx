0406f86adb5d0664356a4dbc3edac2941865b05e
Nhlalo <nhlalonkosi@gmail.com>
2026-02-05 14:27:18 +0200

    feat: add scroll to top handler to root layout component

--- src/components/layout/rootLayout.jsx
+++ src/components/layout/rootLayout.jsx
@@ -18,6 +18,7 @@ export default function RootLayout() {
 
   useEffect(() => {
     const userLocation = mockUserLocation.country;
+    localStorage.setItem("location", userLocation);
     setLocation(userLocation);
   }, []);
 
