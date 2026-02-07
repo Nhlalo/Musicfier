764a1eba0cd90fd508d51a934b4b809b9ed2af54
Nhlalo <nhlalonkosi@gmail.com>
2026-02-07 22:21:07 +0200

    feat: create artist concert component

--- src/pages/concerts/concertDetails/concertDetails.jsx
+++ src/pages/concerts/concertDetails/concertDetails.jsx
@@ -18,3 +18,37 @@ function ArtistInfor({ characterChange }) {
   if (error) return <ErrorMessage />;
   if (data) return <Data artistsInfor={data} />;
 }
+
+function ArtistConcert({
+  artist,
+  location,
+  date,
+  genre,
+  artistImg,
+  concertLink,
+}) {
+  return (
+    <a
+      href={concertLink}
+      aria-label={`Purchase ticket for ${artist}'s concert on ${date} in ${location}`}
+      className={Styles.concertLink}
+    >
+      <img
+        src={artistImg}
+        alt={`${artist}`}
+        loading="lazy"
+        aria-hidden="true"
+        className={Styles.artistImage}
+      />
+      <div aria-hidden="true" className={Styles.concertInfor}>
+        <span className={Styles.date}>
+          <Calendar className={Styles.calendarIcon} />
+          {date}
+        </span>
+        <span className={Styles.artistName}>{artist}</span>
+        <span className={Styles.location}>{location}</span>
+        <span className={Styles.genre}>{genre}</span>
+      </div>
+    </a>
+  );
+}
