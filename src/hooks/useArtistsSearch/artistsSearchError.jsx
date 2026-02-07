deaaa57861dd92deea80b9ab0a711d1c2143c261
Nhlalo <nhlalonkosi@gmail.com>
2026-02-07 20:21:59 +0200

    feat: create concert search error message component

---
+++ src/hooks/useArtistsSearch/artistsSearchError.jsx
@@ -0,0 +1,14 @@
+export default function ErrorMessage() {
+  return (
+    <div className={Styles.errorContainer}>
+      <div aria-hidden="true" className={Styles.searchIconContainer}>
+        <Search className={Styles.searchIcon} />
+      </div>
+      <p className={Styles.noResults}>We couldn't find any results matching</p>
+      <p className={Styles.suggestion}>
+        Please make sure your words are spelled correctly, or try using
+        different keywords
+      </p>
+    </div>
+  );
+}
