bc3b278548073267d523a840205dd96aa63b858b
Nhlalo <nhlalonkosi@gmail.com>
2026-02-07 22:23:31 +0200

    feat: create artist details component

--- src/pages/concerts/concertDetails/concertDetails.jsx
+++ src/pages/concerts/concertDetails/concertDetails.jsx
@@ -52,3 +52,50 @@ function ArtistConcert({
     </a>
   );
 }
+
+//This is the component that will search and display the artist information
+function ArtistDetails({ showFilter }) {
+  const [displayArtistData, setDisplayArtistData] = useState(false);
+  const [inputChange, setInputChange] = useState("");
+  const searchInputRef = useRef(null);
+
+  //Initiate the user's desired concert location
+  const handleArtistSearch = debounce(() => {
+    const inputValue = searchInputRef.current.value;
+
+    //Display if the input value is not empty or only contains one character or is only filled with white spaces
+    if (inputValue.trim().length > 1) {
+      setDisplayArtistData(true);
+      setInputChange(inputValue);
+    } else {
+      setDisplayArtistData(false);
+    }
+  }, 250);
+
+  function handleShowFilter() {
+    showFilter(true);
+  }
+  return (
+    <div className={Styles.searchFilterContainer}>
+      {/* Search results of artist information, error or loading will be displayed when the search input is not empty or filled with white spaces */}
+      {displayArtistData && <ArtistInfor characterChange={inputChange} />}
+      <input
+        type="text"
+        name="concerts"
+        className={Styles.concertInputSearch}
+        placeholder="Artists or Bands"
+        aria-label="Search for an artist's or a bands's concerts"
+        onChange={handleArtistSearch}
+        ref={searchInputRef}
+      />
+      <button
+        type="button"
+        className={Styles.filterBTN}
+        onClick={handleShowFilter}
+      >
+        <Rows3 className={Styles.filterIcon} aria-hidden="true" />
+        <span className={Styles.filter}>Filter</span>
+      </button>
+    </div>
+  );
+}
