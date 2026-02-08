c18e753c1fe37943d6065194a866032aea5acce1
Nhlalo <nhlalonkosi@gmail.com>
2026-02-08 11:14:48 +0200

    feat: implement displaying modal function

---
+++ src/utils/modal.jsx
@@ -0,0 +1,10 @@
+//for any other element besides dialog element
+function displayModal(modal, focusableElements) {
+  // Focus first element
+  const firstFocusable = focusableElements[0];
+  if (firstFocusable) firstFocusable.focus();
+  const lastElement = focusableElements[focusableElements.length - 1];
+
+  // Add focus trap
+  modal.addEventListener("keydown", (e) => trapFocus(e, focusableElements));
+}
