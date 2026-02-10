cbcf50c34676d59f40306db93b3411d576b2d0a2
Nhlalo <nhlalonkosi@gmail.com>
2026-02-10 22:13:30 +0200

    feat: cache the user's location

--- src/components/layout/rootLayout.jsx
+++ src/components/layout/rootLayout.jsx
@@ -45,7 +45,6 @@ export default function RootLayout() {
     localStorage.setItem("location", userLocation);
     setLocation(userLocation);
   }, []);
-
   return (
     <>
       <LocationContext.Provider value={location}>
