b1577a5ca037b11dea1d17a7620abbe52a475d94
Nhlalo <nhlalonkosi@gmail.com>
2026-02-10 21:55:26 +0200

    feat: create search location component

---
+++ src/pages/concerts/sidebar/concertLocation/searchLocation.jsx
@@ -0,0 +1,96 @@
+import { useState, useRef } from "react";
+import { ChevronDown, ChevronUp, MapPinCheck, Search } from "lucide-react";
+import useLocationSearch from "../../../../hooks/useLocationSearch/useLocationSearch";
+import debounce from "../../../../utils/debounce";
+import Data from "../../../../hooks/useLocationSearch/locationSearchData";
+import ErrorMessage from "../../../../hooks/useLocationSearch/locationSearchError";
+import Loading from "../../../../hooks/useLocationSearch/locationSearchLoading";
+import Styles from "../sidebar.module.css";
+
+//This will display the optional concert location as you search for your desired concert location.
+export default function ConcertLocationOptions() {
+  const searchInputBTNRef = useRef(null);
+
+  //This will ensure  that the location searching state data, loading and error is displayed
+  const [displayLocationStatus, setDisplayLocationStatus] = useState(false);
+
+  const [inputChange, setInputChange] = useState("");
+
+  //Determine the visibility of the location input search
+  const [locationSearchVisibility, setLocationSearchVisibility] =
+    useState("hide");
+
+  //show the location search input & results
+  function handleShowLocationSearch() {
+    setLocationSearchVisibility("show");
+  }
+  //hide the location search input & results
+  function handleHideLocationSearch() {
+    setLocationSearchVisibility("hide");
+    searchInputBTNRef.current.value = "";
+    setDisplayLocationStatus(false);
+    searchInputBTNRef.current.value = "";
+    setInputChange("");
+  }
+
+  //Initiate the user's desired concert location
+  const handleLocationSearch = debounce(() => {
+    const inputValue = searchInputBTNRef.current.value;
+
+    //Display if the input value is not empty or only contains one character or is only filled with white spaces
+    if (inputValue.trim().length > 1) {
+      setDisplayLocationStatus(true);
+      setInputChange(inputValue);
+    } else {
+      setDisplayLocationStatus(false);
+    }
+  }, 250);
+
+  return (
+    <>
+      <button
+        type="button"
+        className={Styles.newLocationBTN}
+        onClick={
+          locationSearchVisibility == "hide"
+            ? handleShowLocationSearch
+            : handleHideLocationSearch
+        }
+      >
+        <MapPinCheck aria-hidden="true" />
+        <span className={Styles.newLocation}>New Location</span>{" "}
+        {locationSearchVisibility == "hide" ? (
+          <ChevronDown aria-hidden="true" />
+        ) : (
+          <ChevronUp aria-hidden="true" />
+        )}
+      </button>
+      <div
+        className={
+          locationSearchVisibility == "show"
+            ? Styles.countryInputContainer
+            : Styles.noVisibility
+        }
+      >
+        <Search className={Styles.searchIcon} />
+        <input
+          type="text"
+          name="country"
+          className={Styles.countryInput}
+          ref={searchInputBTNRef}
+          disabled={locationSearchVisibility == "show" ? false : true}
+          onChange={handleLocationSearch}
+        />
+      </div>
+      {displayLocationStatus && <Location characterChange={inputChange} />}
+    </>
+  );
+}
+
+function Location({ characterChange }) {
+  const { data, loading, error } = useLocationSearch(characterChange);
+
+  if (loading) return <Loading />;
+  if (error) return <ErrorMessage />;
+  if (data) return <Data locationData={data} />;
+}
