0a32d59621b7e23a9d7cf261306bb1e6bb343654
Nhlalo <nhlalonkosi@gmail.com>
2026-02-19 00:49:32 +0200

    feat: create mock data of Spotify artist top songs

---
+++ src/data/mock/spotifyArtistTopSongs-mock.jsx
@@ -0,0 +1,53 @@
+const mockTopTracks = [
+  {
+    id: "4iV5W9uYEdYUVa79Axb7Rh",
+    name: "Blinding Lights",
+    previewUrl:
+      "https://p.scdn.co/mp3-preview/2f1b7852ef35523ed5f8e85a6be914e4b6c5e6b6",
+    spotifyUrl: "https://open.spotify.com/track/4iV5W9uYEdYUVa79Axb7Rh",
+    albumImage:
+      "https://i.scdn.co/image/ab67616d0000b2738863bc11d2aa12b54f5aeb36",
+    albumName: "After Hours",
+  },
+  {
+    id: "3AJwUDP919kvQ9QcozQPxg",
+    name: "Save Your Tears",
+    previewUrl:
+      "https://p.scdn.co/mp3-preview/4b7c5a4b5f7d8a9e2f3b4c5d6e7f8a9b0c1d2e3f",
+    spotifyUrl: "https://open.spotify.com/track/3AJwUDP919kvQ9QcozQPxg",
+    albumImage:
+      "https://i.scdn.co/image/ab67616d0000b273c8b7c9d7e8f9a0b1c2d3e4f5",
+    albumName: "After Hours",
+  },
+  {
+    id: "5ctcj0K3HkeW9f9YzH0u6u",
+    name: "Starboy",
+    previewUrl:
+      "https://p.scdn.co/mp3-preview/5d8e9f0a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q",
+    spotifyUrl: "https://open.spotify.com/track/5ctcj0K3HkeW9f9YzH0u6u",
+    albumImage:
+      "https://i.scdn.co/image/ab67616d0000b273e8f9a0b1c2d3e4f5g6h7i8j9",
+    albumName: "Starboy",
+  },
+  {
+    id: "7egj375ez0KtFvVd3l8Ux3",
+    name: "The Hills",
+    previewUrl:
+      "https://p.scdn.co/mp3-preview/6q7r8s9t0u1v2w3x4y5z6a7b8c9d0e1f2g3h4i5j",
+    spotifyUrl: "https://open.spotify.com/track/7egj375ez0KtFvVd3l8Ux3",
+    albumImage:
+      "https://i.scdn.co/image/ab67616d0000b273f0g1h2i3j4k5l6m7n8o9p0q1r",
+    albumName: "Beauty Behind The Madness",
+  },
+  {
+    id: "0VjIjW4GlUZAMYd2vXMi3b",
+    name: "Can't Feel My Face",
+    previewUrl: null, // Some tracks don't have preview URLs
+    spotifyUrl: "https://open.spotify.com/track/0VjIjW4GlUZAMYd2vXMi3b",
+    albumImage:
+      "https://i.scdn.co/image/ab67616d0000b273s2t3u4v5w6x7y8z9a0b1c2d3",
+    albumName: "Beauty Behind The Madness",
+  },
+];
+
+return { mockTopTracks };
