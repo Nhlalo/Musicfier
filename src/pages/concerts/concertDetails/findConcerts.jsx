01a5d3008d421224efcc64f078c742a553db35eb
Nhlalo <nhlalonkosi@gmail.com>
2026-02-18 23:11:39 +0200

    feat: use icon in artist concert component where there is no artist image

--- src/pages/concerts/concertDetails/findConcerts.jsx
+++ src/pages/concerts/concertDetails/findConcerts.jsx
@@ -8,6 +8,7 @@ import formatDate from "../../../utils/dateConversion";
 import ErrorMessage from "../../../hooks/useArtistsSearch/artistsSearchError";
 import Data from "../../../hooks/useArtistsSearch/artistsSearchData";
 import LoadingSpinner from "../../../components/ui/loadingSpinner/loadingSpinner";
+import ImageReplacement from "../../../components/ui/imageReplacement";
 import Styles from "./concertDetails.module.css";
 
 const ArtistInputContext = createContext({});
@@ -87,13 +88,17 @@ function ArtistConcert({
       aria-label={`Purchase ticket for ${artist}'s concert on ${date} in ${location}`}
       className={Styles.concertLink}
     >
-      <img
-        src={artistImg}
-        alt={`${artist}`}
-        loading="lazy"
-        aria-hidden="true"
-        className={Styles.artistImage}
-      />
+      {/* Use an icon when there is no artist image source */}
+      {artistImg && (
+        <img
+          src={artistImg}
+          alt={`${artist}`}
+          loading="lazy"
+          aria-hidden="true"
+          className={Styles.artistImage}
+        />
+      )}
+      {!artistImg && <ImageReplacement iconClass={Styles.artistImage} />}
       <div aria-hidden="true" className={Styles.concertInfor}>
         <span className={Styles.date}>
           <Calendar className={Styles.calendarIcon} />
