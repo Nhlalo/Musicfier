a5a0799ac415ac4ce3cd8faf5cbd4291c8cc449b
Nhlalo <nhlalonkosi@gmail.com>
2026-02-20 23:41:28 +0200

    feat: create spotify similar artists mock data

--- src/data/mock/spotifySimilarArtists-mock.jsx
+++ src/data/mock/spotifySimilarArtists-mock.jsx
@@ -1,42 +1,54 @@
 import { mockArtists } from "./spotifyArtist-mock";
 import ArtistImg from "../../assets/images/artistImg.jpg";
 
+const allArtists = mockArtists.map((artist) => {
+  return {
+    name: artist.artistName,
+    id: artist.spotifyArtistId,
+  };
+});
 const mockSimilarArtists = [
   {
     href: "https://api.spotify.com/v1/artists/1Xyo4u8uXC1ZmMpatF05PJ",
     name: "The Weeknd",
     id: "1Xyo4u8uXC1ZmMpatF05PJ",
-    image: "ArtistImg",
+    image: ArtistImg,
   },
   {
     href: "https://api.spotify.com/v1/artists/6qqNVTkY8uBg9cP3Jd7DAH",
     name: "Billie Eilish",
     id: "6qqNVTkY8uBg9cP3Jd7DAH",
-    image: "ArtistImg",
+    image: ArtistImg,
   },
 ];
 
-const generateMockSimilarArtists = mockArtists.map((artist) => {
-  const similarArtists = {
-    artist: {
-      artistName: artist.artistName,
-      spotifyId: artist.spotifyId,
-    },
-    similarArtists: mockSimilarArtists,
-  };
+function generateMockSimilarArtists(artistsData) {
+  const similarArtists = artistsData.map((artistData) => {
+    const similarArtists = {
+      artist: {
+        artistName: artistData.name,
+        spotifyId: artistData.id,
+      },
+      similarArtists: mockSimilarArtists,
+    };
+
+    return similarArtists;
+  });
 
   return similarArtists;
-});
+}
+
+const mockData = generateMockSimilarArtists(allArtists);
 
 export default function searchMockSimilarArtists(spotifyId) {
-  const artistData = generateMockSimilarArtists.find(
-    (artist) => artist.artist.spotifyId == spotifyId,
+  const artistData = mockData.find(
+    (mockArtist) => mockArtist.artist.spotifyId == spotifyId,
   );
 
   return artistData.similarArtists.map((artist) => ({
-    spotifyHref: artist.href,
+    spotifyLink: artist.href,
     artistName: artist.name,
-    spotifyId: artist.id,
+    spotifyArtistId: artist.id,
     artistImage: artist?.image,
   }));
 }
