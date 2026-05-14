c31075ee54d0d6a4804e482c2ac229d3d9a7c23c
Nhlalo <nhlalonkosi@gmail.com>
2026-05-14 12:22:07 +0200

    refactor: refactor main file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import "./styles/global.css";
-import router from "./route.jsx";
+import { router } from "./route";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
