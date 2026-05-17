2905b986bdb30cb89170a185f40d13abf661569d
Nhlalo <nhlalonkosi@gmail.com>
2026-05-17 16:14:59 +0200

    feat: make the findSong function to be synchronous to making testing easier

--- src/data/__mocks__/audio-recogntion.mock.js
+++ src/data/__mocks__/audio-recogntion.mock.js
@@ -1,21 +1,16 @@
 import { mockCharts } from "./spotify/spotify.mock";
 
 export default function findSong(songName) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const track = mockCharts.find(
-        (song) => song.songName.toLowerCase() == songName.toLowerCase(),
-      );
+  const track = mockCharts.find(
+    (song) => song.songName.toLowerCase() == songName.toLowerCase(),
+  );
 
-      if (track) {
-        resolve({
-          artist: track.artistName,
-          title: track.songName,
-          coverUrl: track.songCover,
-        });
-      } else {
-        resolve(null);
+  return track
+    ? {
+        artist: track.artistName,
+        title: track.songName,
+        coverUrl: track.songCover,
+        id: track.spotifyArtistId,
       }
-    }, 5000);
-  });
+    : null;
 }
